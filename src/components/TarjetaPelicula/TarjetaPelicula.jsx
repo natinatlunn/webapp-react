ecb798786ee0a6b5e5b6590a50883db8923db6e0
Laila <azizi.122844@e.unavarra.es>
2026-03-07 18:46:06 +0100

    Crear componente TarjetaPelicula, añadiendo imagen, nombre, puntuacion y generos de la pelicula

---
+++ src/components/TarjetaPelicula/TarjetaPelicula.jsx
@@ -0,0 +1,40 @@
+import "./TarjetaPelicula.css";
+
+export default function TarjetaPelicula(props) {
+  const pelicula = props.pelicula;
+
+  return (
+    <div className="col" key={pelicula.id}>
+      <div className="card card-pelicula">
+        <img
+          src={`/images/${pelicula.portada}`}
+          className="card-img-top"
+          alt="..."
+        />
+        <div
+          className="card-body text-bg-dark rounded-bottom"
+          style={{
+            backgroundImage: "var(--bs-gradient)",
+          }}
+        >
+          <div className="d-flex justify-content-between align-items-start gap-1">
+            <p className="card-text text-truncate" title={pelicula.titulo}>
+              {pelicula.titulo}
+            </p>
+            <div className="d-flex align-items-center gap-2">
+              <i className="bi bi-heart" />
+              <span className="badge text-bg-dark border">7.0</span>
+            </div>
+          </div>
+          <div className="d-flex gap-2">
+            {pelicula.genero.map((gen) => (
+              <span className="badge rounded-pill text-bg-secondary">
+                {gen}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
