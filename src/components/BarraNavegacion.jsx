7873d58f47df3a8a636216dd06252a79dcfad40d
Laila <azizi.122844@e.unavarra.es>
2026-03-09 21:45:04 +0100

    Cambiar logo de barra de navegacion por uno sin fondo

--- src/components/BarraNavegacion.jsx
+++ src/components/BarraNavegacion.jsx
@@ -1,14 +1,13 @@
-import './BarraNavegacion.css';
-import { useState } from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import logo from '../imagenes/logo.png';
-import FiltroBusqueda from './FiltroBusqueda';
-import InicioSesion from './InicioSesion';
+import "./BarraNavegacion.css";
+import { useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "../imagenes/logo_principal.png";
+import FiltroBusqueda from "./FiltroBusqueda";
+import InicioSesion from "./InicioSesion";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-
 function BarraNavegacion() {
-  const [textoBusqueda, setTextoBusqueda] = useState('');
+  const [textoBusqueda, setTextoBusqueda] = useState("");
 
   const manejarCambioBusqueda = (evento) => {
     setTextoBusqueda(evento.target.value);
@@ -16,21 +15,24 @@ function BarraNavegacion() {
 
   const manejarEnvioBusqueda = (evento) => {
     evento.preventDefault();
-    console.log('Búsqueda enviada:', textoBusqueda);
+    console.log("Búsqueda enviada:", textoBusqueda);
   };
 
- 
   return (
     <>
       <Navbar className="barra-navegacion w-100">
         <div className="zona-logo">
           <img src={logo} alt="logo" className="logo-imagen" />
         </div>
-        <FiltroBusqueda valor={textoBusqueda} onCambio={manejarCambioBusqueda} onEnviar={manejarEnvioBusqueda}/>
+        <FiltroBusqueda
+          valor={textoBusqueda}
+          onCambio={manejarCambioBusqueda}
+          onEnviar={manejarEnvioBusqueda}
+        />
         <InicioSesion />
       </Navbar>
     </>
-  )
+  );
 }
 
 export default BarraNavegacion;
