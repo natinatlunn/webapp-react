49ef8f9ab90a92998baba92553aee8596e1f2654
natinatlunn <127200417+natinatlunn@users.noreply.github.com>
2026-03-11 12:44:04 +0100

    Botón mostrar listado de peliculas

--- src/components/ListadoPeliculas/ListadoPeliculas.jsx
+++ src/components/ListadoPeliculas/ListadoPeliculas.jsx
@@ -1,17 +1,33 @@
 import TarjetaPelicula from "../tarjetaPelicula/TarjetaPelicula";
 import peliculas from "../../data/peliculas.json";
 import "./ListadoPeliculas.css";
+import { useState } from "react";
 
 export default function ListadoPeliculas({ peliculasFiltradas }) {
   const peliculasAMostrar = peliculasFiltradas ?? peliculas;
+  const [mostrar, setMostrar] = useState(false);
+
+  function handleMostrarPeliculas() {
+    setMostrar(!mostrar);
+  }
+
 
   return (
     <div className="peliculas-layout">
-      <div className="peliculas-grid">
-        {peliculasAMostrar.map((pelicula) => {
-          return <TarjetaPelicula key={pelicula.id} pelicula={pelicula} />;
-        })}
-      </div>
+
+      <button onClick={handleMostrarPeliculas} className="btn btn-primary mb-3">
+        {mostrar ? "Ocultar películas" : "Mostrar películas"}
+      </button>
+
+      {mostrar && (
+        <div className="peliculas-grid">
+          {peliculasAMostrar.map((pelicula) => {
+            return <TarjetaPelicula key={pelicula.id} pelicula={pelicula} />;
+          })}
+        </div>
+      )}
+
+
     </div>
   );
 }
