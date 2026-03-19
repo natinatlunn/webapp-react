9c9d80a69313b771d50cb21a10597e7ac190f13d
Laila <azizi.122844@e.unavarra.es>
2026-03-19 23:19:41 +0100

    Refactorizacion y añadir tooltip al componente BotonFavoritos

--- src/components/botonFavoritos/BotonFavoritos.jsx
+++ src/components/botonFavoritos/BotonFavoritos.jsx
@@ -1,12 +1,34 @@
-export default function BotonFavoritos({ favorito, alHacerClic, bloqueado = false }) {
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+
+export default function BotonFavoritos({
+  favorito,
+  alHacerClic,
+  bloqueado = false,
+}) {
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      {bloqueado
+        ? "Inicia sesión para guardar favoritos"
+        : "Guardar en favoritos"}
+    </Tooltip>
+  );
+
   return (
-    <i
-      className={`bi ${favorito ? "bi-heart-fill text-danger" : "bi-heart"} ${
-        bloqueado ? "text-secondary" : ""
-      }`}
-      onClick={alHacerClic}
-      title={bloqueado ? "Inicia sesion para guardar favoritos" : "Guardar en favoritos"}
-      style={{ cursor: "pointer", transition: "all 0.2s" }}
-    />
+    <OverlayTrigger
+      placement="top"
+      delay={{ show: 250, hide: 400 }}
+      overlay={renderTooltip}
+    >
+      <i
+        className={`bi ${favorito ? "bi-heart-fill text-danger" : "bi-heart"} ${
+          bloqueado ? "text-secondary" : ""
+        }`}
+        onClick={bloqueado ? "" : alHacerClic}
+        style={{
+          cursor: bloqueado ? "not-allowed" : "pointer",
+          transition: "all 0.2s",
+        }}
+      />
+    </OverlayTrigger>
   );
 }
